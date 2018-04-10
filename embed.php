f21c89a64f2e95a99af0efc195bfbfb208791cd0
Isaac White <iwhite@vivial.net>
2018-04-10 15:57:03 -0500

    Testing Twitter some more

--- embed.php
+++ embed.php
@@ -6,13 +6,13 @@ $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['DOCUMENT_URI'];
 //var_dump($result);
 ?>
 <!DOCTYPE html>
-<html>
+<html prefix="og: http://ogp.me/ns#">
 <head>
     <script type="text/javascript" async src="<?= $js; ?>"></script>
-    <meta property="twitter:card"        content="player">
-    <meta property="twitter:player"      content="https://youtu.be/zjDQYYOPCAY">
-    <meta property="twitter:site"        content="@vivial_a">
-    <meta property="twitter:creator"     content="@vivial_a">
+    <meta property="og:video"            content="http://youtu.be/zjDQYYOPCAY" />
+    <meta property="og:video:secure_url" content="https://youtu.be/zjDQYYOPCAY" />
+    <meta property="og:video:width"      content="480" />
+    <meta property="og:video:height"     content="240" />
     <meta property="og:title"            content="We came to see Giannis!!">
     <meta property="og:image"            content="https://i.ytimg.com/vi/zjDQYYOPCAY/maxresdefault.jpg">
     <meta property="og:description"      content="Giannis Turns 23, so let's celebrate his 34 greatest plays!">
