7a172b4a9944a29e5b49fe69382f0a38ccd31eff
Isaac White <iwhite@vivial.net>
2018-04-10 14:07:11 -0500

    Check og:image to see if it posts

--- embed.php
+++ embed.php
@@ -10,7 +10,7 @@ $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['DOCUMENT_URI'];
 <head>
     <script type="text/javascript" async src="<?= $js; ?>"></script>
     <meta property="og:video"            content="https://youtu.be/zjDQYYOPCAY">
-    <meta property="og:image"            content="https://img.youtube.com/vi/zjDQYYOPCAY/0.jpg">
+    <meta property="og:image"            content="https://youtu.be/zjDQYYOPCAY">
     <meta property="og:title"            content="We came to see Giannis!!">
     <meta property="og:description"      content="Giannis Turns 23, so let's celebrate his 34 greatest plays!">
     <meta property="og:url"              content="<?= $url; ?>">
