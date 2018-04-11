89d1540c8f89a819fe770cfc57e77b050f43690a
Isaac White <iwhite@vivial.net>
2018-04-11 10:24:41 -0500

    Twitter Stuff

--- js/embed.js
+++ js/embed.js
@@ -11,7 +11,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '240',
         width: '480',
-        videoId: 'zjDQYYOPCAY',
+        videoId: 'kmblhEj9BSA',
         events: {
             'onReady': onPlayerReady
             // 'onStateChange': onPlayerStateChange
