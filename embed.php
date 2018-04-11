1f56155f5d2cf5f59bc9b070d076324568d3aa7a
Isaac White <iwhite@vivial.net>
2018-04-11 10:11:54 -0500

    Twitter Stuff

--- embed.php
+++ embed.php
@@ -15,16 +15,16 @@ $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['DOCUMENT_URI'];
     <meta name="twitter:site"            content="@VivialMedia" />
     <meta name="twitter:description"     content="Giannis Turns 23, We Go For 34!!"/>
     <meta name="twitter:creator"         content="@vivial_a" />
-    <meta name="twitter:player"          content="https://www.youtube.com/embed/zjDQYYOPCAY"/>
+    <meta name="twitter:player"          content="https://www.youtube.com/embed/kmblhEj9BSA"/>
     <meta name="twitter:player:width"    content="480"/>
     <meta name="twitter:player:height"   content="240"/>
-    <meta name="twitter:image"           content="https://i.ytimg.com/vi/zjDQYYOPCAY/maxresdefault.jpg"/>
-    <meta property="og:video:secure_url" content="https://youtu.be/zjDQYYOPCAY" />
+    <meta name="twitter:image"           content="https://i.ytimg.com/vi/kmblhEj9BSA/maxresdefault.jpg"/>
+    <meta property="og:video:secure_url" content="https://youtu.be/kmblhEj9BSA" />
     <meta property="og:video:width"      content="480" />
     <meta property="og:video:height"     content="240" />
     <meta property="og:title"            content="We came to see Giannis!!">
     <meta property="og:description"      content="Giannis Turns 23, so let's celebrate his 34 greatest plays!">
-    <meta "property:image"           content="https://i.ytimg.com/vi/zjDQYYOPCAY/maxresdefault.jpg"/>
+    <meta "property:image"           content="https://i.ytimg.com/vi/kmblhEj9BSA/maxresdefault.jpg"/>
     <meta property="og:url"              content="<?= $url ?>">
     <meta property="fb:app_id"           content="514918008605706">
     <meta property="og:type"             content="article">
